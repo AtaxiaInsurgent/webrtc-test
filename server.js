83d51186552fea923967809fd1123fed58e34cad
Wong Peng Seong <kmarcusxiii@aeonsparx.com>
2018-07-19 09:48:17 +0800

    - Removed configuration for Access-Control-Allow-Origin - Added key 'description' in emit in handleIceCandidate

--- server.js
+++ server.js
@@ -7,19 +7,6 @@ var socketIO = require('socket.io');
 
 var fileServer = new(nodeStatic.Server)();
 var app = http.createServer(function(req, res) {
-  // // Website you wish to allow to connect
-  // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
-
-  // // Request methods you wish to allow
-  // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // // Request headers you wish to allow
-  // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // // Set to true if you need the website to include cookies in the requests sent
-  // // to the API (e.g. in case you use sessions)
-  // res.setHeader('Access-Control-Allow-Credentials', true);
-
   fileServer.serve(req, res);
 }).listen(8080);
 
